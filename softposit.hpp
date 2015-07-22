d0d844e1c524b181c4374ad4ba369a1a318eada7
uuplusu <justin.seeley.cn@gmail.com>
2015-07-22 13:41:35 +0800

    compiles ok, run it?

--- softposit.hpp
+++ softposit.hpp
@@ -63,6 +63,8 @@ arma::mat sinkhornSlack(arma::mat M);
 
 int numMatches(arma::mat assignMat);
 
+std::tuple<arma::umat, arma::mat> maxPosRatio(arma::mat assignMat);
+
 // at least it will find something
 boost::optional< std::tuple<Pose_type, match_type> > softposit(
   const std::vector<point2di_type>& imagePts,
@@ -106,7 +108,7 @@ boost::optional< std::tuple<Pose_type, match_type> > softposit(
                  _centeredImage.begin(),
                  [&caminfo](const point2di_type & _pt){
                    return point2d_type((point2d_type{double(_pt[0]), double(_pt[1])} - caminfo.center)/caminfo.focalLength);
-                 });;
+                 });
 
   arma::mat centeredImage = arma::zeros<arma::mat>(_centeredImage.size(),2);
   for (int j=0; j<centeredImage.n_cols; ++j)
@@ -116,15 +118,16 @@ boost::optional< std::tuple<Pose_type, match_type> > softposit(
     }
   }
 
-  auto homogeneousWorldPts = my::transform<std::vector<arma::vec4>>(worldPts, [](const auto & _pt){return arma::vec4{_pt[0], _pt[1], _pt[2], 1}; });
-
+  auto homogeneousWorldPts = arma::zeros<arma::mat>(worldPts.size(), 4).eval();
+  for (int i=0; i<worldPts.size(); ++i)
+  {
+    homogeneousWorldPts.row(i) = arma::rowvec{worldPts[i][0], worldPts[i][1], worldPts[i][2],1};
+  }
 
   auto pose = initpose;
 
-  auto wk = arma::vec(my::transform<std::vector<double>>(homogeneousWorldPts,
-                                                         [&pose](const auto& _pt)->double{
-                                                           return arma::dot(_pt,  arma::vec4 {pose.rot(2,0), pose.rot(2,1), pose.rot(2,2),1});
-                                                         }));
+  arma::mat wk = homogeneousWorldPts * arma::vec4 {pose.rot(2,0), pose.rot(2,1), pose.rot(2,2),1};
+
   arma::vec4 r1T = {pose.rot(0,0)/pose.trans(2), pose.rot(0,1)/pose.trans(2), pose.rot(0,2)/pose.trans(2), pose.trans(0)/pose.trans(2)};
   arma::vec4 r2T = {pose.rot(1,0)/pose.trans(2), pose.rot(1,1)/pose.trans(2), pose.rot(1,2)/pose.trans(2), pose.trans(1)/pose.trans(2)};
 
@@ -140,10 +143,8 @@ boost::optional< std::tuple<Pose_type, match_type> > softposit(
 
   while (beta < betaFinal && !assignConverged)
   {
-    auto projectedU = arma::vec(my::transform<std::vector<double>>(homogeneousWorldPts, [&r1T](const auto& _p)->double{ return arma::dot(_p, r1T);}));          // Column N-vector.
-
-    auto projectedV = arma::vec(my::transform<std::vector<double>>(homogeneousWorldPts, [&r2T](const auto& _p)->double{ return arma::dot(_p, r2T);}));          // Column N-vector.
-
+    auto projectedU = homogeneousWorldPts * r1T;
+    auto projectedV = homogeneousWorldPts * r2T;
 
     auto replicatedProjectedU = imageOnes * projectedU.t();
     auto replicatedProjectedV = imageOnes * projectedV.t();
@@ -159,84 +160,92 @@ boost::optional< std::tuple<Pose_type, match_type> > softposit(
 
 
     assignMat = sinkhornSlack (assignMat);    // My "improved" Sinkhorn.
-    numMatchPts = numMatches(assignMat);
-    sumNonslack = arma::sum(assignMat(0,0,nbImagePts-1,nbWorldPts-1));
+    auto numMatchPts = numMatches(assignMat);
+    auto sumNonslack = arma::accu(assignMat.submat(0,0,nbImagePts-1,nbWorldPts-1));
 
-    auto summedByColAssign = arma::sum(assignMat(0, 0, nbImagePts-1, nbWorldPts-1)).eval();
-    arma::mat sumSkSkT = amra::zeros<arma::mat>(4, 4);
+    auto summedByColAssign = arma::sum(assignMat.submat(0, 0, nbImagePts-1, nbWorldPts-1)).eval();
+    arma::mat sumSkSkT = arma::zeros<arma::mat>(4, 4);
     for(auto  k = 0; k<nbWorldPts; ++k){
       sumSkSkT = sumSkSkT + summedByColAssign(k) * homogeneousWorldPts.row(k).t() * homogeneousWorldPts.row(k);
     }
-    if cond(sumSkSkT) > 1e10
-             std::cout<<('sumSkSkT is ill-conditioned, termininating search.')<<std::endl;
-    return boost::none;
+    if (cond(sumSkSkT) > 1e10){
+      std::cout<<"sumSkSkT is ill-conditioned, termininating search."<<std::endl;
+      return boost::none;
+    }
 
     arma::mat objectMat = arma::inv(sumSkSkT);                           // Inv(L), a 4x4 matrix.
     poseConverged = 0;                              // Initialize for POSIT loop.
-    count = 0;
+    auto pose_iter_count = 0;
 
     // Save the previouse pose vectors for convergence checks.
     auto r1Tprev = r1T;
     auto r2Tprev = r2T;
 
-    arma::vec weightedUi(4, 0) ;
-    arma::vec weightedVi(4, 0) ;
-    for (int j=0;j<nbImagePts; ++j){
-      for (int k=0; k<nbWorldPts; ++k){
-        weightedUi = weightedUi + assignMat(j,k) * wk(k) * centeredImage(j,0) * homogeneousWorldPts(k,arma::span::all).t();
-        weightedVi = weightedVi + assignMat(j,k) * wk(k) * centeredImage(j,1) * homogeneousWorldPts(k,arma::span::all).t();
+    double Tx, Ty, Tz;
+    arma::vec r1, r2, r3;
+    double delta;
+
+    while (poseConverged == false & pose_iter_count < maxCount){
+
+      arma::vec weightedUi(4, 0) ;
+      arma::vec weightedVi(4, 0) ;
+      for (int j=0;j<nbImagePts; ++j){
+        for (int k=0; k<nbWorldPts; ++k){
+          weightedUi = weightedUi + assignMat(j,k) * wk(k) * centeredImage(j,0) * homogeneousWorldPts(k,arma::span::all).t();
+          weightedVi = weightedVi + assignMat(j,k) * wk(k) * centeredImage(j,1) * homogeneousWorldPts(k,arma::span::all).t();
+        }
       }
+
+      r1T= objectMat * weightedUi;
+      r2T = objectMat * weightedVi;
+
+      arma::mat U, V;
+      arma::vec s;
+      arma::mat X(3,2);
+      X.col(0) = r1T(arma::span(0,2));
+      X.col(1) = r2T(arma::span(0,2));
+
+      arma::svd(U,s,V, X);
+
+      arma::mat A = U * arma::mat("1 0; 0 1; 0 0") * V.t();
+
+      auto r1 = A.col(0);
+      auto r2 = A.col(1);
+      arma::vec r3 = arma::cross(r1,r2);
+      Tz = 2 / (s(0) + s(1));
+      Tx = r1T(3) * Tz;
+      Ty = r2T(3) * Tz;
+      auto r3T= arma::vec{r3[0], r3[1], r3[2], Tz};
+
+      auto r1T = arma::vec{r1[0], r1[1], r1[2], Tx}/Tz;
+      auto r2T = arma::vec{r2[0], r2[1], r2[3], Ty}/Tz;
+
+
+      wk = homogeneousWorldPts * r3T /Tz;
+
+      delta = sqrt(arma::accu(assignMat.submat(0, 0, nbImagePts-1, nbWorldPts-1) % distMat)/nbWorldPts);
+      poseConverged = delta < maxDelta;
+
+      stats.push_back(arma::vec(
+                        std::vector<double>{
+                          beta ,delta ,double(numMatchPts)/nbWorldPts , double(sumNonslack)/nbWorldPts,
+                            arma::accu(arma::square(r1T-r1Tprev)) + arma::accu(arma::square(r2T-r2Tprev))
+                            }));
+
+      pose_iter_count = pose_iter_count + 1;
     }
-
-    r1T= objectMat * weightedUi;
-    r2T = objectMat * weightedVi;
-
-    arma::mat U, V;
-    arma::vec s;
-    arma::mat X(3,2);
-    X(0) = r1T(arma::span(0,2));
-    X(1) = r2T(arma::span(0,2));
-
-    arma::svd(U,s,V, X);
-
-  arma:mat A = U * arma::mat("1 0; 0 1; 0 0") * V.t();
-
-    auto r1 = A(0);
-    auto r2 = A(1);
-    arma::vec r3 = arma::cross(r1,r2);
-    double Tz = 2 / (S(0) + S(1));
-    auto Tx = r1T(3) * Tz;
-    auto Ty = r2T(3) * Tz;
-    auto r3T= arma::vec{r3[0], r3[1], r3[2], Tz};
-
-    auto r1T = arma::vec{r1[0], r1[1], r1[2], Tx}/Tz;
-    auto r2T = arma::vec{r2[0], r2[1], r2[3], Ty}/Tz;
-
-
-    wk = homogeneousWorldPts * r3T /Tz;
-
-    double delta = arma::sqrt(arma::sum(assignMat(0, 0, nbImagePts-1, nbWorldPts-1) % distMat)/nbWorldPts);
-    poseConverged = delta < maxDelta;
-
-    stats(betaCount,0) = beta;
-    stats(betaCount,1) = delta;
-    stats(betaCount,2) = numMatchPts/nbWorldPts;
-    stats(betaCount,3) = sumNonslack/nbWorldPts;
-    stats(betaCount,4) = arma::sum(arma::square(r1T-r1Tprev)) + arma::sum(arma::square(r2T-r2Tprev));
-
-    count = count + 1;
 
     beta = betaUpdate * beta;
     betaCount = betaCount + 1;
     assignConverged = poseConverged && betaCount > minBetaCount;
 
-    pose.trans = arma::vec{Tx, Ty, Tz}
+    pose.trans = arma::vec{Tx, Ty, Tz};
     pose.rot.row(0) = r1.t();
     pose.rot.row(1) = r2.t();
     pose.rot.row(2) = r3.t();
 
     foundPose = (delta < maxDelta && betaCount > minBetaCount);
- }
+  }
 
 
   return make_tuple(initpose, match_type());
@@ -255,23 +264,23 @@ arma::mat sinkhornSlack(arma::mat M)
 
   auto fMdiffSum = fEpsilon2 + 1;
 
-  while(abs(fMdiffSum) > fEpsilon2 && iNumSinkIter < iMaxIterSinkhorn){
+  while(fabs(fMdiffSum) > fEpsilon2 && iNumSinkIter < iMaxIterSinkhorn){
     auto Mprev = M; // % Save M from previous iteration to test for loop termination
 
-   // Col normalization (except outlier row - do not normalize col slacks against each other)
-    auto McolSums = arma::sum(M); // Row vector.
+    // Col normalization (except outlier row - do not normalize col slacks against each other)
+    arma::rowvec McolSums = arma::sum(M); // Row vector.
     McolSums(nbCols-1) = 1; // Don't normalize slack col terms against each other.
     auto McolSumsRep = arma::ones<arma::vec>(nbRows) * McolSums ;
     M = M / McolSumsRep;
 
     // Row normalization (except outlier row - do not normalize col slacks against each other)
-    auto MrowSums = arma::sum(M, 1); // Column vector.
+    arma::colvec MrowSums = arma::sum(M, 1); // Column vector.
     MrowSums(nbRows-1) = 1; // Don't normalize slack row terms against each other.
     auto MrowSumsRep = MrowSums * arma::ones<arma::rowvec>(nbCols);
-    M = M ./ MrowSumsRep;
+    M = M / MrowSumsRep;
 
     iNumSinkIter=iNumSinkIter+1;
-    fMdiffSum=arma::sum(arma::abs(M-Mprev));
+    fMdiffSum=arma::accu(arma::abs(M-Mprev));
   }
 
   normalizedMat = M;
@@ -291,22 +300,110 @@ int numMatches(arma::mat assignMat)
 
   for (int k = 0; k< nmodpnts; ++k){
 
-    int imax;
-    auto vmax = assignMat.cols(k).max(imax);
+    arma::uword imax;
+    auto vmax = assignMat.col(k).max(imax);
 
     if(imax == assignMat.n_rows - 1) continue; // Slack value is maximum in this column.
 
-
-    std::vector<int> other_cols;
+    std::vector<arma::uword> other_cols;
     other_cols.reserve(assignMat.n_cols-1);
     for (int i=0; i<assignMat.n_cols; ++i)
       if (i!=k) other_cols.push_back(i);
-    if (arma::all(vmax > assignMat(imax,other_cols)))
-            num = num + 1;              // This value is maximal in its row & column.
+    if (arma::all(arma::all(vmax > assignMat.submat(arma::uvec{imax},arma::uvec(other_cols)))))
+      num = num + 1;              // This value is maximal in its row & column.
   }
   return num;
 }
 
 
+arma::mat sinkhornImp(arma::mat M){
+  arma::mat normalizedMat;
+
+  auto iMaxIterSinkhorn=60;          // In PAMI paper
+  auto fEpsilon2 = 0.001;            // Used in termination of Sinkhorn Loop.
+
+  auto iNumSinkIter = 0;
+  auto nbRows = M.n_rows;
+  auto nbCols = M.n_cols;
+
+  auto fMdiffSum = fEpsilon2 + 1;    // Set "difference" from last M matrix above the loop termination threshold
+
+// Get the positions and ratios to slack of the nonslack elements that are maximal in their row and column.
+  arma::umat posmax;
+  arma::mat ratios;
+  std::tie(posmax, ratios) = maxPosRatio(M);
+
+  while(fabs(fMdiffSum) > fEpsilon2 && iNumSinkIter < iMaxIterSinkhorn)
+  {
+    auto Mprev = M;  // Save M from previous iteration to test for loop termination
+
+    // Col normalization (except outlier row - do not normalize col slacks
+    // against each other)
+    arma::rowvec McolSums = arma::sum(M, 0);  // Row vector.
+    McolSums(nbCols-1) = 1;  // Don't normalize slack col terms against each other.
+
+    auto McolSumsRep = arma::ones<arma::vec>(nbRows) * McolSums ;
+    M = M / McolSumsRep;
+
+    // Fix values in the slack column.
+    for (auto i=0; i< posmax.n_rows; ++i){
+        M(posmax(i,0), nbCols-1) = ratios(i,0)*M(posmax(i,0),posmax(i,1));
+    }
+    // Row normalization (except outlier row - do not normalize col slacks against each other)
+    arma::vec MrowSums = arma::sum(M, 1);  // Column vector.
+    MrowSums(nbRows-1) = 1;  // Don't normalize slack row terms against each other.
+
+    auto MrowSumsRep = MrowSums * arma::ones<arma::rowvec>(nbCols);
+    M = M / MrowSumsRep;
+
+    // Fix values in the slack row.
+    for (auto i=0; i< posmax.n_rows; ++i){
+        M(nbRows-1,posmax(i,1)) = ratios(i,1)*M(posmax(i,0),posmax(i,1));
+      }
+    iNumSinkIter=iNumSinkIter+1;
+    fMdiffSum=arma::accu(arma::abs(M-Mprev));
+
+  }
+  normalizedMat = M;
+
+  return normalizedMat;
+}
+
+
+std::tuple<arma::umat, arma::mat> maxPosRatio(arma::mat assignMat)
+{
+//    function [pos, ratios] = maxPosRatio(assignMat)
+  arma::umat pos;
+  arma::mat ratios;
+
+  auto nrows =  assignMat.n_rows; //size(assignMat,1);
+  auto ncols = assignMat.n_cols; //size(assignMat,2);
+  auto nimgpnts  = nrows - 1;
+  auto nmodpnts = ncols - 1;
+
+// Iterate over all columns of assignMat.
+  for (auto k=0u; k<nmodpnts; ++k){
+    arma::uword imax;
+    auto vmax = assignMat.col(k).max(imax);
+
+    if (imax == nrows-1) continue;                       // Slack value is maximum in this column.
+
+// Check if the max value in the column is maximum within its row.
+
+    std::vector<arma::uword> other_cols;
+    for (int i=0; i<assignMat.n_cols; ++i) if (i!=k) other_cols.push_back(i);
+
+    if (arma::all(arma::all(vmax > assignMat.submat(arma::uvec{imax},arma::uvec(other_cols))))){
+      pos = arma::join_cols(pos, arma::umat(arma::urowvec{imax, k}));
+
+      // Compute the ratios to row and column slack values.
+      auto rr = assignMat(imax,ncols-1)/assignMat(imax,k);
+      auto cr = assignMat(nrows-1,k)/assignMat(imax,k);
+      ratios = arma::join_cols(ratios, arma::mat(arma::rowvec{rr, cr}));
+    }
+  }
+
+  return std::make_tuple(pos, ratios);
+}
 
 #endif /* uu_recreate_wheel_imp_SOFTPOSIT_H */
